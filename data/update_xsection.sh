d71731967573ef2f25bdd4a922644877eed91ff0
Harish Potti <harish.potti@cern.ch>
2015-07-15 14:17:20 +0000

    Updated file in util folder
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@235308 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- data/update_xsection.sh
+++
@@ -1,9 +0,0 @@
-
-svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/TopPhys/TopPhysUtils/TopDataPreparation/trunk/data/
-mv $(pwd)/data/XSection-MC15-13TeV-fromSusyGrp.data .
-g++ xsection_reader.cpp
-./a.out
-rm XSection-MC15-13TeV-fromSusyGrp.data
-rm -rf ./data/
-echo "Successfully Updated Cross-sections and K-factors"
-~              
