9cfd4cae378706e1368d356fbe1b5ebc4295299b
Giuseppe Salamanna <Giuseppe.Salamanna@cern.ch>
2015-07-26 10:09:02 +0000

    adding new ilumicalc lumi profile for 50 ns period
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@235394 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- Root/ttHMultileptonLooseEventSaver.cxx
+++ Root/ttHMultileptonLooseEventSaver.cxx
@@ -159,11 +159,11 @@ void ttHMultileptonLooseEventSaver::initialize(std::shared_ptr<top::TopConfig> c
     m_purwtool = new CP::PileupReweightingTool("prw");
     std::vector<std::string> confFiles;
     std::vector<std::string> lcalcFiles;
-    confFiles.push_back("prw.410000.partial.e3698_s2608_s2183_r6630_r6264.7vii15.root");
-    lcalcFiles.push_back("ilumicalc_histograms_None_266904-267639.root");
-    top::check(dynamic_cast<CP::PileupReweightingTool&>(*m_purwtool).setProperty( "ConfigFiles", confFiles),"m_purwtool won't set confFiles");
-    top::check(dynamic_cast<CP::PileupReweightingTool&>(*m_purwtool).setProperty( "LumiCalcFiles", lcalcFiles), "m_purwtool won't set lcalcFiles");
-    top::check(m_purwtool->initialize(),"m_purwtool won't initialize");
+    //confFiles.push_back("prw.410000.partial.e3698_s2608_s2183_r6630_r6264.7vii15.root");
+    //lcalcFiles.push_back("ilumicalc_histograms_None_266904-267639.root");
+    //top::check(dynamic_cast<CP::PileupReweightingTool&>(*m_purwtool).setProperty( "ConfigFiles", confFiles),"m_purwtool won't set confFiles");
+    //top::check(dynamic_cast<CP::PileupReweightingTool&>(*m_purwtool).setProperty( "LumiCalcFiles", lcalcFiles), "m_purwtool won't set lcalcFiles");
+    //top::check(m_purwtool->initialize(),"m_purwtool won't initialize");
     
     //TRIGGER PART
     // Trigger decision tool. 
@@ -512,7 +512,7 @@ void ttHMultileptonLooseEventSaver::saveEvent(const top::Event& event){
     }
   }
   
-  top::check(m_purwtool->apply( *event.m_info ), "Failed to apply pileup weight");
+  //top::check(m_purwtool->apply( *event.m_info ), "Failed to apply pileup weight");
 
   //event info
   m_eventNumber = event.m_info->eventNumber();
