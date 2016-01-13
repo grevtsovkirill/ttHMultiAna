0ac26c70abf5b95c5ef2c28ad55d0b396bc2ed7b
David Hohn <david.hohn@cern.ch>
2016-01-13 09:18:56 +0000

    loose TTVA cuts
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@237380 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- Root/ElectronLikelihoodMC15.cxx
+++ Root/ElectronLikelihoodMC15.cxx
@@ -17,6 +17,27 @@ namespace ttHMultilepton {
   
   bool ElectronLikelihoodMC15::passTTVACuts(const xAOD::Electron& el) const
   {
+
+    if( !el.isAvailable<float>("d0sig") ){
+      std::cout << "d0 significance not found for electron. "
+		<< "Maybe no primary vertex? Won't accept." << std::endl;
+      return false;
+    }
+  
+    float d0sig = el.auxdataConst<float>("d0sig");
+    if( std::abs(d0sig) >= 10 )
+      return false;
+  
+    if( !el.isAvailable<float>("delta_z0_sintheta") ){
+      std::cout << "delta z0*sin(theta) not found for electron. "
+		<< "Maybe no primary vertex? Won't accept." << std::endl;
+      return false;
+    }
+  
+    float delta_z0_sintheta = el.auxdataConst<float>("delta_z0_sintheta");
+    if( std::abs(delta_z0_sintheta) >= 2 )
+      return false;
+    
     return true;
   }
   
