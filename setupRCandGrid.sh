71338765c8e4391ce9ffb833a8bba0fc3a6d9046
Tamara Vazquez Schroeder <tamara.vazquez.schroeder@cern.ch>
2016-06-07 16:21:29 +0000

    check out TopAnalysis tag that fixes normalisation problem
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@238976 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- setupRCandGrid.sh
+++ setupRCandGrid.sh
@@ -5,6 +5,7 @@ lsetup "rcsetup Top,2.4.6" panda rucio pyami
 #lsetup knows in which order to do these
 
 rc checkout_pkg atlasoff/PhysicsAnalysis/AnalysisCommon/PMGTools/trunk
+rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/tags/TopAnalysis-00-04-36
 rc build
 
 echo "Alright - done."
