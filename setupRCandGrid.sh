e7f3312a722ad353357ee711eb3251f6ac34ab91
David Hohn <david.hohn@cern.ch>
2016-07-03 14:47:04 +0000

    fix tau bug
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@239402 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- setupRCandGrid.sh
+++ setupRCandGrid.sh
@@ -7,7 +7,7 @@ lsetup "rcsetup Top,2.4.13" panda rucio pyami
 rc checkout_pkg atlasoff/PhysicsAnalysis/AnalysisCommon/PMGTools/tags/PMGTools-00-00-01
 rc checkout_pkg atlasoff/PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/tags/ElectronEfficiencyCorrection-00-01-62
 rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/TopPhysUtils/TopDataPreparation/trunk
-rc checkout_pkg atlasoff/PhysicsAnalysis/TauID/TauAnalysisTools/tags/TauAnalysisTools-00-02-14
+rc checkout_pkg atlasoff/PhysicsAnalysis/TauID/TauAnalysisTools/tags/TauAnalysisTools-00-02-16
 rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/xAOD/TopCorrections/tags/TopCorrections-00-00-86
 rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/xAOD/TopCPTools/tags/TopCPTools-00-01-01
 rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/xAOD/TopAnalysis/tags/TopAnalysis-00-04-49
