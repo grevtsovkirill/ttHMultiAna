ea47316579ec40aa98e6eae008d6ddc718860afa
Robert Wolff <robert.wolff@cern.ch>
2016-07-21 22:23:41 +0000

    prepare for v19: workaround in TopObjectSelectionTools
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@239877 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- setupRCandGrid.sh
+++ setupRCandGrid.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 echo "Setting up all the things"
 
-lsetup "rcsetup Top,2.4.15" panda rucio pyami
+lsetup "rcsetup Top,2.4.16" panda rucio pyami
 #lsetup knows in which order to do these
+rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/xAOD/TopSystematicObjectMaker/tags/TopSystematicObjectMaker-00-03-59
+rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/xAOD/TopObjectSelectionTools/tags/TopObjectSelectionTools-00-03-85
+
+# manually add following line to TopObjectSelectionTools/Root/ElectronLikelihoodMC15.cxx line 107:
+#   if (!(el.caloCluster())) return false;
 
 rc build
 
