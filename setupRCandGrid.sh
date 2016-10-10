8493cfc2c2423d9dbfa2f618e492ea740a20e0d9
Robert Wolff <robert.wolff@cern.ch>
2016-10-10 13:46:48 +0000

    added bad electron handling without crashing
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@240477 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- setupRCandGrid.sh
+++ setupRCandGrid.sh
@@ -5,6 +5,10 @@ echo "Setting up all the things"
 
 lsetup "rcsetup Top,2.4.19" panda rucio pyami
 
+# for bad electrons in HIGG8D1 with no matched clusters (e.g. run 302872)
+rc checkout $(rc version | grep TopObjectSelectionTools)
+sed -i 's/\/\/This stops a crash/if (!(el.caloCluster())) return false;/' TopObjectSelectionTools/Root/ElectronLikelihoodMC15.cxx
+
 rc build
 
 echo "Alright - done."
