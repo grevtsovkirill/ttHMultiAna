ce29dfd3999f11ef347904f023465f1c70e6c7e9
Robert Wolff <robert.wolff@cern.ch>
2016-10-12 11:35:03 +0000

    fix rc checkout in setupRCandGrid.sh
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@240479 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- setupRCandGrid.sh
+++ setupRCandGrid.sh
@@ -6,7 +6,7 @@ echo "Setting up all the things"
 lsetup "rcsetup Top,2.4.19" panda rucio pyami
 
 # for bad electrons in HIGG8D1 with no matched clusters (e.g. run 302872)
-rc checkout $(rc version | grep TopObjectSelectionTools)
+rc checkout_pkg $(rc version | grep TopObjectSelectionTools)
 sed -i 's/\/\/This stops a crash/if (!(el.caloCluster())) return false;/' TopObjectSelectionTools/Root/ElectronLikelihoodMC15.cxx
 
 rc build
