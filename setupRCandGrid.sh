40ca730a91700c55e45dce4b80890f0374b6dd96
Robert Wolff <robert.wolff@cern.ch>
2016-11-03 15:42:44 +0000

    added new dilep and lep+tau triggers, update for AnalysisTop, 2.4.21
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@240505 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- setupRCandGrid.sh
+++ setupRCandGrid.sh
@@ -3,14 +3,11 @@ echo "Setting up all the things"
 
 #lsetup knows in which order to do these
 
-lsetup "rcsetup Top,2.4.19" panda rucio pyami
+lsetup "rcsetup Top,2.4.21" panda rucio pyami
 
 # for bad electrons in HIGG8D1 with no matched clusters (e.g. run 302872)
 rc checkout_pkg $(rc version | grep TopObjectSelectionTools)
 sed -i 's/\/\/This stops a crash/if (!(el.caloCluster())) return false;/' TopObjectSelectionTools/Root/ElectronLikelihoodMC15.cxx
-
-# Vgamma samples
-rc checkout_pkg atlasoff/PhysicsAnalysis/TopPhys/TopPhysUtils/TopDataPreparation/tags/TopDataPreparation-00-08-29
 
 rc build
 
