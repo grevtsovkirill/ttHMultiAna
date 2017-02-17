f1a9b6b12bf42429542c17e4e55b812601c06eda
Robert Wolff <robert.wolff@cern.ch>
2017-02-17 19:21:17 +0000

    add priority1 sys ntuples for submission
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys-hsg8/Physics/Higgs/HSG8/AnalysisCode/multileptons/xAOD/ttHMultiAna/trunk@240951 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- scripts/MC_PostICHEP.py
+++ scripts/MC_PostICHEP.py
@@ -943,3 +943,28 @@ if len(noShowerDatasets) > 0:
     for ds in set(noShowerDatasets):
         print ds
     raise RuntimeError("Datasets without shower.")
+
+
+xsecfile1 = open(os.getenv('ROOTCOREBIN') + '/../ttHMultilepton/util/Xsection13TeV_tth_bkg_v1.txt')
+xsecfile2 = open(os.getenv('ROOTCOREBIN') + '/../ttHMultilepton/util/Xsection13TeV_tth_sig_v1.txt')
+content = xsecfile1.readlines() + xsecfile2.readlines()
+sys_fullSim = []
+sys_fastSim = []
+for TopSample in TopExamples.grid.availableDatasets.values():
+    for sample in TopSample.datasets:
+        dsid = sample.split('.')[1]
+        fastSim = "_a766" in sample
+        line = [line for line in content if dsid in line]
+        if len(line) != 1:
+            print "WARNING: dsid " + dsid + " has " + str(len(line)) + " matches in Xsection13TeV_tth_*_v1.txt"
+        else:
+            priority = line[0].split()[6]
+            #print dsid+": "+priority+" fS:"+str(fastSim)
+            if priority == '1':
+                if fastSim:
+                    sys_fastSim.append(sample)
+                else:
+                    sys_fullSim.append(sample)
+#TopExamples.grid.Add('sys_fullSim').datasets = sys_fullSim
+#TopExamples.grid.Add('sys_fastSim').datasets = sys_fastSim
+import MC_PostICHEP_priority1
