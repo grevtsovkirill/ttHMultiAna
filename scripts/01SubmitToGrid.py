21084d53312c33f6028ae241275df6271bb93ed2
Robert Wolff <robert.wolff@cern.ch>
2017-07-07 15:47:44 +0200

    new samples

--- scripts/01SubmitToGrid.py
+++ scripts/01SubmitToGrid.py
@@ -13,7 +13,7 @@ for configFile in configFiles:
 
 config = TopExamples.grid.Config()
 config.code          = 'top-xaod'
-config.gridUsername  = 'awebb'
+config.gridUsername  = 'rwolff'
 config.excludedSites = 'ANALY_CONNECT,ANALY_CONNECT_SHORT'#'ANALY_GOEGRID'
 config.noSubmit      = False
 config.mergeType     = 'Default' #'None', 'Default' or 'xAOD'
@@ -37,21 +37,49 @@ TopExamples.grid.convertAODtoTOPQ('DAOD_HIGG8D1','p2949', VJets_fullSim)
 fullSim = All_fullSim + ttbar_fullSim + VJets_fullSim
 fastSim = All_fastSim + ttbar_fastSim
 
+TopExamples.grid.Add('newSamples').datasets = [
+'mc15_13TeV.410276.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee_mll_1_5.merge.AOD.e6087_s2726_r7772_r7676',
+'mc15_13TeV.410277.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu_mll_1_5.merge.AOD.e6087_s2726_r7772_r7676',
+'mc15_13TeV.410278.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_tttautau_mll_1_5.merge.AOD.e6087_s2726_r7772_r7676',
+'mc15_13TeV.410397.MadGraphPythia8EvtGen_ttbar_wbee_MEN30LO_A14N23LO.merge.AOD.e6086_s2726_r7772_r7676',
+'mc15_13TeV.410398.MadGraphPythia8EvtGen_ttbar_wbmumu_MEN30LO_A14N23LO.merge.AOD.e6086_s2726_r7772_r7676',
+'mc15_13TeV.410399.MadGraphPythia8EvtGen_ttbar_wbtautau_MEN30LO_A14N23LO.merge.AOD.e6086_s2726_r7772_r7676',
+'mc15_13TeV.410155.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttW.merge.AOD.e5070_s2726_r7772_r7676',
+'mc15_13TeV.410156.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZnunu.merge.AOD.e5070_s2726_r7772_r7676',
+'mc15_13TeV.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.merge.AOD.e5070_s2726_r7772_r7676',
+'mc15_13TeV.410218.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee.merge.AOD.e5070_s2726_r7772_r7676',
+'mc15_13TeV.410219.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu.merge.AOD.e5070_s2726_r7772_r7676',
+'mc15_13TeV.410220.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_tttautau.merge.AOD.e5070_s2726_r7772_r7676',
+]
+newSamples = TopExamples.grid.Samples(['newSamples'])
+TopExamples.grid.convertAODtoTOPQ('DAOD_HIGG8D1','p3201', newSamples)
+TopExamples.grid.Add('extra').datasets = [
+'mc15_13TeV.343365.aMcAtNloPythia8EvtGen_A14_NNPDF23_NNPDF30ME_ttH125_dilep.merge.AOD.e4706_s2726_r7772_r7676',
+'mc15_13TeV.343366.aMcAtNloPythia8EvtGen_A14_NNPDF23_NNPDF30ME_ttH125_semilep.merge.AOD.e4706_s2726_r7772_r7676',
+'mc15_13TeV.343367.aMcAtNloPythia8EvtGen_A14_NNPDF23_NNPDF30ME_ttH125_allhad.merge.AOD.e4706_s2726_r7772_r7676',
+]
+extra = TopExamples.grid.Samples(['extra'])
+TopExamples.grid.convertAODtoTOPQ('DAOD_HIGG8D1','p2949', extra)
+
 #Systematics
 config.maxNFilesPerJob = '1'
 config.memory = ''
 
-config.suffix       = '2017-06-09.Sys_v29_noCFT'
+config.suffix       = '2017-06-09.Sys_v29_noCFT_skim'
 config.settingsFile = 'generic_config-mc15-Sys.txt'
 #TopExamples.grid.submit(config,fullSim)
+TopExamples.grid.submit(config,extra)
 config.settingsFile = 'generic_config-mc15-Sys_fastSim.txt'
 #TopExamples.grid.submit(config,fastSim)
+#TopExamples.grid.submit(config,noCFT_ttbar_fastSim)
 
-config.suffix       = '2017-06-09.Sys_v29_CFT'
+config.suffix       = '2017-06-09.Sys_v29_CFT_skim'
 config.settingsFile = 'generic_config-mc15-Sys_CFT.txt'
 #TopExamples.grid.submit(config,fullSim)
+TopExamples.grid.submit(config,extra)
 config.settingsFile = 'generic_config-mc15-Sys_CFT_fastSim.txt'
 #TopExamples.grid.submit(config,fastSim)
+#TopExamples.grid.submit(config,CFT_ttbar_fastSim)
 
 ####################################################################################
 #Nominal
