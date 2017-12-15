ac763689183e0de5995197e3c80a234d892a783a
nazlim <merve.nazlim.agaras@cern.ch>
2017-12-15 17:57:28 +0100

    added simple script to run

---
+++ scripts/mc16r21.py
@@ -0,0 +1,10 @@
+
+import TopExamples.grid
+import TopExamples.ami
+
+
+# just a test sample processed with 21.0.19.2+DerivationFrameworkTop-00-04-06
+TopExamples.grid.Add("newsamples").datasets = [
+    "mc16_13TeV.410501.PowhegPythia8EvtGen_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_HIGG8D1.e5458_s3126_r9781_r9778_p3371",
+    ]
+
