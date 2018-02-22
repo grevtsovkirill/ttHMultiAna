417f5b342731f0707d34aab314a306c594709268
Albert Francis Casha <albert.casha@cern.ch>
2018-02-22 12:56:36 +0000

    Upload New File

---
+++ scripts/Grid/dataR21_byPeriod.py
@@ -0,0 +1,44 @@
+import TopExamples.grid
+import TopExamples.ami
+
+import grid
+
+
+grid.Add("samples_period_data15").datasets = [
+
+	"data15_13TeV.periodG.physics_Main.PhysCont.DAOD_HIGG8D1.grp15_v01_p3372",
+	"data15_13TeV.periodJ.physics_Main.PhysCont.DAOD_HIGG8D1.grp15_v01_p3372",
+	"data15_13TeV.periodE.physics_Main.PhysCont.DAOD_HIGG8D1.grp15_v01_p3372",
+	"data15_13TeV.periodF.physics_Main.PhysCont.DAOD_HIGG8D1.grp15_v01_p3372",
+	"data15_13TeV.periodH.physics_Main.PhysCont.DAOD_HIGG8D1.grp15_v01_p3372",
+	"data15_13TeV.periodD.physics_Main.PhysCont.DAOD_HIGG8D1.grp15_v01_p3372",
+
+]
+
+grid.Add("samples_period_data16").datasets = [
+
+	"data16_13TeV.periodA.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodE.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodG.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodC.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodK.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodL.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodB.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodI.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodD.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+	"data16_13TeV.periodF.physics_Main.PhysCont.DAOD_HIGG8D1.grp16_v01_p3372",
+
+]
+
+grid.Add("samples_period_data17").datasets = [
+
+	"data17_13TeV.periodB.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodE.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodH.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodC.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodF.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodI.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodD.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+	"data17_13TeV.periodK.physics_Main.PhysCont.DAOD_HIGG8D1.grp17_v01_p3372",
+
+]
