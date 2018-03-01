3c532c490290c9853958b98f262854efea2018cf
Rohin Thampilali Narayan <rohin.thampilali.narayan@cern.ch>
2018-03-01 23:53:09 +0000

    Update ttHMultileptonLooseEventSaver.cxx

--- Root/ttHMultileptonLooseEventSaver.cxx
+++ Root/ttHMultileptonLooseEventSaver.cxx
@@ -1873,7 +1873,7 @@ void ttHMultileptonLooseEventSaver::finalize()
 
       //m_outputFile->cd();
 
-      myTree->GetEntry(0) // This populates names_lhe. Can use this for booking histograms and adding bin labels
+      myTree->GetEntry(0); // This populates names_lhe. Can use this for booking histograms and adding bin labels
  
       m_outputFile->cd("loose");
       TH1D* count_histo_lhe_weights = new TH1D("Count_LHE", "LHE weights", names_lhe->size(), -0.5, names_lhe->size() - 0.5);
